085a89df8a1da9c114b27280d203e87866fe2097
Kalwador <szpila.piotrek@gmail.com>
2017-02-25 11:53:08 +0100

    mih

--- Arkanoid3d/Assets/Scripts/Ball.cs
+++ Arkanoid3d/Assets/Scripts/Ball.cs
@@ -1,32 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Ball : MonoBehaviour
-{
-
-    public float ballInitialVelocity = 800f;
 
 
-    private Rigidbody rb;
-    private bool ballInPlay;
-   
-    void Awake()
+
+    public class Ball : MonoBehaviour
     {
 
-        rb = GetComponent<Rigidbody>();
+        public float ballInitialVelocity = 600f;
 
+
+        private Rigidbody rb;
+        private bool ballInPlay;
+
+        void Awake()
+        {
+
+            rb = GetComponent<Rigidbody>();
+
+        }
+
+        void Update()
+        {
+            
+
+                if (Input.GetButtonDown("Fire1") && ballInPlay == false)
+                {
+                    transform.parent = null;
+                    ballInPlay = true;
+                    rb.isKinematic = false;
+                    rb.AddForce(new Vector3(ballInitialVelocity, ballInitialVelocity, 0));
+                }
+            
+
+        }
+        public bool BallInPlay()
+    {
+        return ballInPlay;
     }
-
-    void Update()
-    {
-      
-            if (Input.GetButtonDown("Fire1") && ballInPlay == false)
-            {
-                transform.parent = null;
-                ballInPlay = true;
-                rb.isKinematic = false;
-                rb.AddForce(new Vector3(ballInitialVelocity, ballInitialVelocity, 0));
-            }
         
     }
-}
